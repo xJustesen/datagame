fb1e34c67085b8d3df9daa1646993960ae971db8
Jens Bo Justesen <jensbojustesen@gmail.com>
2021-09-18 15:49:28 +0200

    Update layout and histogram binning

--- main.py
+++ main.py
@@ -18,12 +18,10 @@ np.random.seed(314)
 
 @st.cache(suppress_st_warning=True)
 def get_samples(sampler, n, key):
-    if key == 2:
-        _samples = sampler(n)
-        _samples = _samples[(_samples > -25) & (_samples < 25)]
-        return _samples
-    else:
-        return sampler(n)
+    s = sampler(n)
+    if key == 2:  # limit samples for better plotting
+        s = s[(s > -25) & (s < 25)]
+    return s
 
 
 def get_pdf(key, x):
@@ -42,10 +40,10 @@ def get_pdf(key, x):
         raise NotImplementedError
 
 
-def plot_distribution(_samples, _reveal):
+def plot_distribution(samples, plot_pdf):
     fig, ax = plt.subplots()
-    _, bins, _ = ax.hist(_samples, bins=50, density=True)
-    if _reveal:
+    _, bins, _ = ax.hist(samples, bins=100, density=True)
+    if plot_pdf:
         ax.plot(bins, get_pdf(distribution_key, bins))
     return fig
 
@@ -53,28 +51,39 @@ def plot_distribution(_samples, _reveal):
 st.title('Guess the Distribution!')
 
 # set input parameters
-distribution_key = st.selectbox(
-    'Select a distribution!',
-    DISTRIBUTIONS.keys()
+# make radio buttons appear horizontal and centered on the page
+st.write(
+    '<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: center}</style>', unsafe_allow_html=True
 )
-st.write('You selected distribution: ', distribution_key)
+distribution_key = st.radio(
+    label='Select one of the five secret distributions:',
+    options=DISTRIBUTIONS.keys(),
+    index=0,
+)
 n_samples = st.slider(
     'Select the number of samples:',
-    min_value=0,
+    min_value=100,
     max_value=1000,
-    value=0,
-    step=100
+    value=100,
+    step=10
 )
-st.write(f'You selected {n_samples} number of samples')
 
 # sample distribution
 name, _sampler = DISTRIBUTIONS[distribution_key]
-samples = get_samples(_sampler, n_samples, distribution_key)
+_samples = get_samples(_sampler, n_samples, distribution_key)
 
-# reveal answer
-reveal = st.checkbox('Reveal Distribution?')
-if reveal:
-    st.write('It was a '+name+' distribution!')
+# get guess and determine if it is correct or not
+guess = st.text_input('Input the name of the distribution you think it is:', value="").strip()
+if guess.lower() == name.lower():
+    reveal = True
+    st.write(f'Congratulations! You guessed *{guess}* distribution, which is correct!')
+else:
+    reveal = False
+    if guess == "":
+        st.write(f'Please write a guess in the box above!')
+    else:
+        st.write(f'You guessed *{guess}* distribution, which is incorrect :(. Please guess again!')
 
-fig = plot_distribution(samples, reveal)
-st.pyplot(fig)
+# plot distribution
+_fig = plot_distribution(_samples, reveal)
+st.pyplot(_fig)
